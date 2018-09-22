c903b34b4f0d2ef4665113a28403152c189ccfa8
Sabin <sabin@localhost.localdomain>
2018-09-22 17:14:52 +0200

    Exress modified to make socket works

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const path = require('path'); // this modules is already installed with node js and it resolve the paths of folders (watch the examples)
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 
 const publicPath = path.join(__dirname, '/../public');
 //console.log(__dirname+'/../public'); // example
 //console.log(publicPath);            // example
-
+var port = 3000 || process.env.PORT;
 var app = express();
 app.use(express.static(publicPath));
 
-var port = 3000 || process.env.PORT;
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+  });
+});
 
 
 
-app.listen(port, () => {
-  console.log('Server Listening on port 3000');
+
+
+server.listen(port, () => {
+  console.log('Server Listening on port '+ port);
 });
